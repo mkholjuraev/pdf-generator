338477f33652fcfe9f4141988a654dc41ebd056f
Levente Berky <levente.berky@gmail.com>
2021-10-20 10:33:00 +0200

    Add 3 missing charts

--- src/pdf/schemas/index.js
+++ src/pdf/schemas/index.js
@@ -1,8 +1,18 @@
 import affectedHostsByPlaybook from './affectedHostsByPlaybook';
 import changesMade from './changesMade';
 import playbookRunRate from './playbookRunRate';
+import hostsByOrganization from './hostsByOrganization';
+import templatesExplorer from './templatesExplorer';
+import jobsTasksByOrganization from './jobsTasksByOrganization';
 
-const reports = [affectedHostsByPlaybook, changesMade, playbookRunRate];
+const reports = [
+  affectedHostsByPlaybook,
+  changesMade,
+  playbookRunRate,
+  hostsByOrganization,
+  templatesExplorer,
+  jobsTasksByOrganization,
+];
 
 export const getReport = (searchSlug) =>
   reports.find(({ slug }) => slug === searchSlug) ?? reports[0];
