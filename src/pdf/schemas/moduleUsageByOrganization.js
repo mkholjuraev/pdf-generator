5bf753b4817d0ed3f443be905582965f00321768
Salma Kochay <skochay330@gmail.com>
2021-10-29 11:13:48 -0400

    indentation fixes

--- src/pdf/schemas/moduleUsageByOrganization.js
+++ src/pdf/schemas/moduleUsageByOrganization.js
@@ -1,10 +1,10 @@
 import {
-ChartKind,
-ChartType,
-ChartLegendOrientation,
-ChartLegendPosition,
-ChartTopLevelType,
-ChartThemeColor,
+  ChartKind,
+  ChartType,
+  ChartLegendOrientation,
+  ChartLegendPosition,
+  ChartTopLevelType,
+  ChartThemeColor,
 } from 'react-json-chart-builder';
 
 const slug = 'module_usage_by_organization';
@@ -15,88 +15,88 @@ const description =
   'The number of job template and task runs for a specified set of Ansible modules, grouped by organizations from Ansible Controller.\n\nYou can use this report to find which organizations are using particular modules, helping you to check things like adoption of purpose-built modules of particular teams.';
 
 const tableHeaders = [
-{ key: 'id', value: 'ID' },
-{ key: 'name', value: 'Organization name' },
-{ key: 'host_task_count', value: 'Tasks count' },
-{ key: 'host_task_changed_count', value: 'Changed tasks count'},
-{ key: 'host_task_ok_count', value: 'Successful tasks count'},
-{ key: 'host_task_failed_count', value: 'Failed tasks count'},
-{ key: 'host_task_unreachable_count', value: 'Unreachable tasks count'}
+  { key: 'id', value: 'ID' },
+  { key: 'name', value: 'Organization name' },
+  { key: 'host_task_count', value: 'Tasks count' },
+  { key: 'host_task_changed_count', value: 'Changed tasks count' },
+  { key: 'host_task_ok_count', value: 'Successful tasks count' },
+  { key: 'host_task_failed_count', value: 'Failed tasks count' },
+  { key: 'host_task_unreachable_count', value: 'Unreachable tasks count' }
 ];
 
 const schemaFnc = (
-label,
-y,
-xTickFormat,
+  label,
+  y,
+  xTickFormat,
 ) => [
     {
-    id: 1,
-    kind: ChartKind.wrapper,
-    type: ChartTopLevelType.chart,
-    parent: null,
-    props: {
+      id: 1,
+      kind: ChartKind.wrapper,
+      type: ChartTopLevelType.chart,
+      parent: null,
+      props: {
         height: 500,
         padding: {
-        top: 70,
-        left: 100,
+          top: 70,
+          left: 100,
         },
         domainPadding: {
-        y: 25,
+          y: 25,
         },
         themeColor: ChartThemeColor.multiOrdered,
-    },
-    xAxis: {
+      },
+      xAxis: {
         label: 'Date',
         tickFormat: xTickFormat,
         style: {
-        axisLabel: {
+          axisLabel: {
             padding: 50,
+          },
         },
-        },
-    },
-    yAxis: {
+      },
+      yAxis: {
         tickFormat: 'formatNumberAsK',
         showGrid: true,
         label,
         style: {
-        axisLabel: {
+          axisLabel: {
             padding: 55,
+          },
         },
-        },
-    },
-    api: {
+      },
+      api: {
         url: '',
         params: {},
-    },
-    legend: {
+      },
+      legend: {
         interactive: false,
         orientation: ChartLegendOrientation.vertical,
         position: ChartLegendPosition.right,
-    },
+      },
     },
     {
-    id: 2,
-    kind: ChartKind.group,
-    parent: 1,
-    template: {
+      id: 2,
+      kind: ChartKind.group,
+      parent: 1,
+      template: {
         id: 0,
         kind: ChartKind.simple,
         type: ChartType.line,
         parent: 0,
         props: {
-        x: 'created_date',
-        y,
+          x: 'created_date',
+          y,
         },
+      },
     },
-    },
-];
+  ];
 
 const reportParams = {
-slug,
-name,
-description,
-tableHeaders,
-schemaFnc,
+  slug,
+  name,
+  description,
+  tableHeaders,
+  schemaFnc,
 };
 
 export default reportParams;
--- src/pdf/schemas/mostUsedModules.js
+++ src/pdf/schemas/mostUsedModules.js
@@ -1,10 +1,10 @@
 import {
-ChartKind,
-ChartType,
-ChartLegendOrientation,
-ChartLegendPosition,
-ChartTopLevelType,
-ChartThemeColor,
+  ChartKind,
+  ChartType,
+  ChartLegendOrientation,
+  ChartLegendPosition,
+  ChartTopLevelType,
+  ChartThemeColor,
 } from 'react-json-chart-builder';
 
 const slug = 'most_used_modules';
@@ -15,88 +15,89 @@ const description =
   'The number of job template and task runs, grouped by Ansible module usage.\n\nYou can use this report to find which modules are being used the most across your automation, helping you to check things like organization-wide adoption of purpose-built modules over potentially less performant, catch-all solutions.';
 
 const tableHeaders = [
-{ key: 'id', value: 'ID' },
-{ key: 'name', value: 'Module name' },
-{ key: 'host_task_count', value: 'Tasks count' },
-{ key: 'host_task_changed_count', value: 'Changed tasks count'},
-{ key: 'host_task_ok_count', value: 'Successful tasks count'},
-{ key: 'host_task_failed_count', value: 'Failed tasks count'},
-{ key: 'host_task_unreachable_count', value: 'Unreachable tasks count'}
+  { key: 'id', value: 'ID' },
+  { key: 'name', value: 'Module name' },
+  { key: 'host_task_count', value: 'Tasks count' },
+  { key: 'host_task_changed_count', value: 'Changed tasks count' },
+  { key: 'host_task_ok_count', value: 'Successful tasks count' },
+  { key: 'host_task_failed_count', value: 'Failed tasks count' },
+  { key: 'host_task_unreachable_count', value: 'Unreachable tasks count' }
 ];
 
 const schemaFnc = (
-label,
-y,
-xTickFormat,
+  label,
+  y,
+  xTickFormat,
 ) => [
     {
-    id: 1,
-    kind: ChartKind.wrapper,
-    type: ChartTopLevelType.chart,
-    parent: null,
-    props: {
+      id: 1,
+      kind: ChartKind.wrapper,
+      type: ChartTopLevelType.chart,
+      parent: null,
+      props: {
         height: 500,
         padding: {
-        top: 70,
-        left: 100,
+          top: 70,
+          left: 100,
         },
         domainPadding: {
-        y: 25,
+          y: 25,
         },
         themeColor: ChartThemeColor.multiOrdered,
-    },
-    xAxis: {
+      },
+      xAxis: {
         label: 'Date',
         tickFormat: xTickFormat,
         style: {
-        axisLabel: {
+          axisLabel: {
             padding: 50,
+          },
         },
-        },
-    },
-    yAxis: {
+
+      },
+      yAxis: {
         tickFormat: 'formatNumberAsK',
         showGrid: true,
         label,
         style: {
-        axisLabel: {
+          axisLabel: {
             padding: 55,
+          },
         },
-        },
-    },
-    api: {
+      },
+      api: {
         url: '',
         params: {},
-    },
-    legend: {
+      },
+      legend: {
         interactive: false,
         orientation: ChartLegendOrientation.vertical,
         position: ChartLegendPosition.right,
-    },
+      },
     },
     {
-    id: 2,
-    kind: ChartKind.group,
-    parent: 1,
-    template: {
+      id: 2,
+      kind: ChartKind.group,
+      parent: 1,
+      template: {
         id: 0,
         kind: ChartKind.simple,
         type: ChartType.line,
         parent: 0,
         props: {
-        x: 'created_date',
-        y,
+          x: 'created_date',
+          y,
         },
+      },
     },
-    },
-];
+  ];
 
 const reportParams = {
-slug,
-name,
-description,
-tableHeaders,
-schemaFnc,
+  slug,
+  name,
+  description,
+  tableHeaders,
+  schemaFnc,
 };
 
 export default reportParams;
