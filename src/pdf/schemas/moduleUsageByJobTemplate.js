dbf9fe570e0896198de51cf0e9c77bfbd22af43a
Levente Berky <levente.berky@gmail.com>
2021-11-02 11:50:56 +0100

    Added legend word wrapping at 18 char to pdf charts

--- src/pdf/schemas/moduleUsageByJobTemplate.js
+++ src/pdf/schemas/moduleUsageByJobTemplate.js
@@ -72,6 +72,8 @@ const schemaFnc = (
         interactive: false,
         orientation: ChartLegendOrientation.vertical,
         position: ChartLegendPosition.right,
+        turncateAt: 18,
+        wrapText: true,
       },
     },
     {
