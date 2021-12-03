421f8af7dc1f7aaf6905262437e42ea3df76135b
Aparna Karve <akarve@redhat.com>
2021-12-03 14:52:36 -0800

    ensure if all the aws config parameters exist prior
    In ephemeral environments, there is no aws config available

--- server/logger.js
+++ server/logger.js
@@ -4,8 +4,6 @@ import fs from 'fs';
 
 const clowderConfigJson = process.env.ACG_CONFIG;
 
-const NAMESPACE = fs.readFileSync("/var/run/secrets/kubernetes.io/serviceaccount/namespace").toString().trim();
-
 const { combine, timestamp, label, printf } = format;
 
 const myFormat = printf(({ message, label, timestamp }) => {
@@ -24,21 +22,26 @@ const logger = createLogger({
 if (clowderConfigJson) {
   const clowderConfig = JSON.parse(fs.readFileSync(clowderConfigJson));
 
-  const config = {
-    logGroupName: clowderConfig.logging.cloudwatch.logGroup,
-    logStreamName: NAMESPACE,
-    createLogGroup: false,
-    createLogStream: true,
-    awsConfig: {
-      accessKeyId: clowderConfig.logging.cloudwatch.accessKeyId,
-      secretAccessKey: clowderConfig.logging.cloudwatch.secretAccessKey,
-      region: clowderConfig.logging.cloudwatch.region
-    },
-    formatLog: function (item) {
-      return item.level + ': ' + item.message + ' ' + JSON.stringify(item.meta)
-    }
+  if (clowderConfig.logging.cloudwatch.accessKeyId &&
+      clowderConfig.logging.cloudwatch.secretAccessKey &&
+      clowderConfig.logging.cloudwatch.region) {
+      const namespace = fs.readFileSync("/var/run/secrets/kubernetes.io/serviceaccount/namespace").toString().trim();
+      const config = {
+        logGroupName: clowderConfig.logging.cloudwatch.logGroup,
+        logStreamName: namespace,
+        createLogGroup: false,
+        createLogStream: true,
+        awsConfig: {
+          accessKeyId: clowderConfig.logging.cloudwatch.accessKeyId,
+          secretAccessKey: clowderConfig.logging.cloudwatch.secretAccessKey,
+          region: clowderConfig.logging.cloudwatch.region
+        },
+        formatLog: function (item) {
+          return item.level + ': ' + item.message + ' ' + JSON.stringify(item.meta)
+        }
+      }
+      logger.add(CloudWatchTransport, config);
   }
-  logger.add(CloudWatchTransport, config);
 }
 
 logger.level = process.env.LOG_LEVEL || "info";
