72f6ad9537a024c75f91d7fef835d9b00e559818
Levente Berky <levente.berky@gmail.com>
2021-12-07 11:37:34 +0100

    Extracted from the server the specific fetching code

---
+++ server/aapScript.js
@@ -0,0 +1,91 @@
+import { PDFNotImplementedError, PDFRequestError } from './errors';
+import reports from '../src/schemas';
+import axios from 'axios';
+
+const getData = (baseURL, headers, queryParams) => {
+  const { offset, limit, ...rest } = queryParams;
+  const url = baseURL;
+
+  const urlQuery = new URLSearchParams(url.search);
+  urlQuery.set('offset', offset);
+  urlQuery.set('limit', limit);
+  url.search = urlQuery.toString();
+
+  return axios
+    .post(url.toString(), rest, { headers })
+    .then((response) => response.data)
+    .catch((err) => {
+      throw new PDFRequestError(err);
+    });
+};
+
+const getExtraData = (baseUrl, headers, queryParams, dataSize) => {
+  const maxOffset = dataSize - queryParams.limit > 100 ? 100 : dataSize;
+
+  let promises = [];
+  for (let offset = 0; offset < maxOffset; offset += 25) {
+    promises = [
+      ...promises,
+      getData(baseUrl, headers, {
+        ...queryParams,
+        offset,
+        limit: 25,
+        include_others: false,
+      }),
+    ];
+  }
+
+  return Promise.all(promises)
+    .then((responses) =>
+      responses.map(({ meta }) => meta.legend).reduce((a, b) => a.concat(b), [])
+    )
+    .catch((err) => {
+      throw new PDFRequestError(err);
+    });
+};
+
+const getParamsForGenerator = async ({
+  slug,
+  label,
+  y,
+  x_tick_format: xTickFormat,
+  // For data fetching
+  queryParams,
+  showExtraRows,
+  apiHost,
+  apiPort,
+  endpointUrl,
+  // Added by the pdf server
+  rhIdentity,
+}) => {
+  if (!reports.find(({ layoutProps }) => layoutProps.slug === slug)) {
+    throw new PDFNotImplementedError();
+  }
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'x-rh-identity': rhIdentity,
+  };
+
+  const { sort_options, sort_order, ...restQuery } = queryParams;
+  const fastApiUrl = new URL(endpointUrl, `http://${apiHost}:${apiPort}`);
+  fastApiUrl.search = new URLSearchParams({ sort_options, sort_order });
+
+  const data = await getData(fastApiUrl, headers, restQuery);
+
+  const extraDataLegend =
+    showExtraRows === 'True'
+      ? await getExtraData(fastApiUrl, headers, restQuery, data.meta.count)
+      : [];
+
+  return {
+    data,
+    extraData: { meta: { legend: extraDataLegend } },
+    slug,
+    label,
+    y,
+    xTickFormat,
+  };
+};
+
+export default getParamsForGenerator;
--- server/server.js
+++ server/server.js
@@ -3,20 +3,14 @@ import fs from 'fs';
 import path from 'path';
 import cors from 'cors';
 import atob from 'atob';
-import axios from 'axios';
 
 import React from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
 import logger from './logger';
 import App from '../src/App';
 import generatePdf from '../browser';
-import reports from '../src/schemas';
-import {
-  PDFNotImplementedError,
-  PDFNotFoundError,
-  SendingFailedError,
-  PDFRequestError,
-} from './errors';
+import { PDFNotFoundError, SendingFailedError } from './errors';
+import getParamsForGenerator from './aapScript';
 
 const PORT = process.env.PORT || 8000;
 const APIPrefix = '/api/tower-analytics/v1';
@@ -53,86 +47,14 @@ app.post(`${APIPrefix}/generate_pdf/`, async (req, res) => {
 
   const tenant = JSON.parse(atob(rhIdentity))['identity']['internal']['org_id'];
   const url = `http://localhost:${PORT}`;
-  const { offset, limit, sort_options, sort_order } = req.body.queryParams;
 
   try {
-    if (
-      !reports.find(({ layoutProps }) => layoutProps.slug === req.body.slug)
-    ) {
-      throw new PDFNotImplementedError();
-    }
-    const queryParams = req.body.queryParams;
-    const fastApiUrl = `http://${req.body.apiHost}:${req.body.apiPort}${req.body.endpointUrl}?&sort_by=${sort_options}:${sort_order}`;
-    const headers = {
-      'Content-Type': 'application/json',
-      'x-rh-identity': rhIdentity,
-    };
+    // Custom script to get the params for our app
+    const params = await getParamsForGenerator({ ...req.body, rhIdentity });
 
-    // get data for current report displayed in UI
-    const promise = async () => {
-      return await axios
-        .post(`${fastApiUrl}&offset=${offset}&limit=${limit}`, queryParams, {
-          headers: headers,
-        })
-        .then(async (response) => {
-          return response.data;
-        })
-        .catch((err) => {
-          throw new PDFRequestError(err);
-        });
-    };
+    // Generate the pdf
+    const pathToPdf = await generatePdf(url, params);
 
-    const currentData = await promise().then((res) => {
-      return res;
-    });
-
-    let extraData = null;
-    if (req.body.showExtraRows === 'True') {
-      const excludeOthers = { include_others: false };
-      const promiseCount =
-        currentData.meta.count - limit > 100 ? 4 : currentData.meta.count / 25;
-      let newOffset = 0;
-      // get extra data
-      const promises = [];
-      for (let i = 0; i < promiseCount; i++) {
-        promises.push(
-          axios.post(
-            `${fastApiUrl}&offset=${newOffset}&limit=25`,
-            { ...excludeOthers, ...queryParams },
-            { headers: headers }
-          )
-        );
-        newOffset += 25;
-      }
-      const allPromises = async () => {
-        let data = { meta: {} };
-        return await Promise.all(promises)
-          .then((response) => {
-            data.meta.legend = response.map((p) => p.data.meta.legend);
-            data.meta.legend = data.meta.legend.reduce(
-              (a, b) => a.concat(b),
-              []
-            );
-            return data;
-          })
-          .catch((err) => {
-            throw new PDFRequestError(err);
-          });
-      };
-
-      extraData = await allPromises().then((res) => {
-        return res;
-      });
-    }
-
-    const pathToPdf = await generatePdf(url, {
-      data: currentData,
-      extraData: extraData,
-      slug: req.body.slug,
-      label: req.body.label,
-      y: req.body.y,
-      xTickFormat: req.body.x_tick_format,
-    });
     const pdfFileName = pathToPdf.split('/').pop();
 
     if (!fs.existsSync(pathToPdf)) {
