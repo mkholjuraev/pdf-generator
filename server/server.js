5826ebd2da3c4085518c8a0827c36ea3db22f938
Aparna Karve <akarve@redhat.com>
2022-01-26 16:08:32 -0800

    Fixes 413 error noticed on payloads > 100 kb

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const APIPrefix = '/api/tower-analytics/v1';
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '5mb'}));
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 app.use('^/$', (_req, res, _next) => {
