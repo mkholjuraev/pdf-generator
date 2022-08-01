0d7b565294a6e9379d4f6a2d05dcffa306615813
Martin Maroši <marvusm.mmi@gmail.com>
2022-08-01 13:46:47 +0200

    Get compliance URL from env

--- server/data-access/call-service.ts
+++ server/data-access/call-service.ts
@@ -1,29 +1,15 @@
 import axios, { AxiosRequestHeaders, AxiosRequestConfig } from 'axios';
 import ServiceNames from './service-names';
+import config from '../config';
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 // TODO: Base it on https://gitlab.cee.redhat.com/service/app-interface/-/blob/master/data/products/insights/environments/production.yml and load trough clowder
 const AVAIABLE_SERVICES = {
-  [ServiceNames.compliance]: {
-    host: 'compliance-service.compliance-prod.svc.cluster.local',
-    port: 8000,
-  },
-  [ServiceNames.vulnerability]: {
-    host: 'foo.bar',
-    port: 8000,
-  },
-  [ServiceNames.advisor]: {
-    host: 'foo.bar',
-    port: 8000,
-  },
-  [ServiceNames.demo]: {
-    host: 'foo.bar',
-    port: 8000,
-  },
-  [ServiceNames.vulnerabilitiesSystem]: {
-    host: 'foo.bar',
-    port: 8000,
-  },
+  [ServiceNames.compliance]: config.COMPLIANCE_URL,
+  [ServiceNames.vulnerability]: 'foo.bar:8000',
+  [ServiceNames.advisor]: 'foo.bar:8000',
+  [ServiceNames.demo]: 'foo.bar:8000',
+  [ServiceNames.vulnerabilitiesSystem]: 'foo.bar:8000',
 };
 
 export type APIDescriptor<T = Record<string, unknown>, R = unknown> = {
@@ -46,7 +32,9 @@ function prepareServiceCall<T = Record<string, unknown>>(
   if (!serviceConfig) {
     throw new Error(`Trying to reach unusupported service ${service}!`);
   }
-  const URL = `${serviceConfig.host}:${serviceConfig.port}${path}`;
+  const URL = `${serviceConfig}${
+    path.match(/^/) ? path : path.replace(/^/, '/')
+  }`;
   return async (headers, options) => {
     let data;
     if (IS_DEVELOPMENT) {
