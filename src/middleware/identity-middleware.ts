f9e03886dd3ddabe0202f6b3d9bb5324eb0b4387
Martin Maroši <marvusm.mmi@gmail.com>
2022-09-12 13:01:07 +0200

    Handle missing identity header

--- src/middleware/identity-middleware.ts
+++ src/middleware/identity-middleware.ts
@@ -6,11 +6,13 @@ import config from '../common/config';
 const identityMiddleware: Handler = (req, _res, next) => {
   try {
     const rhIdentity = req.headers[config.IDENTITY_HEADER_KEY];
-    const identityObject = JSON.parse(
-      Buffer.from(rhIdentity, 'base64').toString()
-    );
-    httpContext.set(config.IDENTITY_HEADER_KEY, rhIdentity);
-    httpContext.set(config.IDENTITY_CONTEXT_KEY, identityObject);
+    if (rhIdentity) {
+      const identityObject = JSON.parse(
+        Buffer.from(rhIdentity, 'base64').toString()
+      );
+      httpContext.set(config.IDENTITY_HEADER_KEY, rhIdentity);
+      httpContext.set(config.IDENTITY_CONTEXT_KEY, identityObject);
+    }
     next();
   } catch (error) {
     next(error);
